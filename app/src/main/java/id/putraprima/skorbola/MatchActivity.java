f40d9d6f60c17f4cbb6c2258081c06e4122bacfd
Putra Prima Arhandi <putraprima@gmail.com>
2020-02-23 08:19:43 +0700

    starter code

---
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -0,0 +1,19 @@
+package id.putraprima.skorbola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+        //TODO
+        //1.Menampilkan detail match sesuai data dari main activity
+        //2.Tombol add score menambahkan memindah activity ke scorerActivity dimana pada scorer activity di isikan nama pencetak gol
+        //3.Dari activity scorer akan mengirim kembali ke activity matchactivity otomatis nama pencetak gol dan skor bertambah +1
+        //4.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang beserta nama pencetak gol ke ResultActivity, jika seri di kirim text "Draw",
+    }
+}
